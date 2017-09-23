aee3c71cc663673ff33c2eb125dbb64c4b93c5b7
Alexander Kozko <Alexander Kozko>
2017-09-23 12:04:32 +0500

    morphological analyser

--- Sources/NetMorpher/NetMorpher.fs
+++ Sources/NetMorpher/NetMorpher.fs
@@ -1,4 +1,24 @@
 namespace NetMorpher
 
+open StringUtils
+open NetMorpher.Prediction
+open NetMorpher.MapBuilder
+
 type NetMorpher() = 
-    member this.X = "F#"
+    let path =  __SOURCE_DIRECTORY__ 
+    let data, index = BinaryDataLoader.loadAll |> TrieMapBuilder.buildMap
+    
+    member this.Index = index
+
+    member this.AnnalyzeToken (token: string) = 
+        let lowerToken = token.NormalizeToken()
+        let result = data.FuzzySearch <| lowerToken.Reverse()
+        match result.Found with
+            | true -> result.Records 
+                        |> Seq.collect (fun x-> x.Record |> Seq.map (fun r -> r.GetFinal())) 
+                        |> Seq.toList
+            | false -> Predictor.MakePredictions result.Records result.Prefix lowerToken
+
+    member this.LemmatizeToken token = 
+        let lemmas = this.AnnalyzeToken token
+        lemmas.Head.Lemma
---
+++ Sources/NetMorpher/Prediction.fs
@@ -0,0 +1,31 @@
+﻿namespace NetMorpher.Prediction
+
+open NetMorpher.Types
+open System
+open NetMorpher.Structures.TrieMap
+open StringUtils
+
+module Predictor =
+    let createLemmaHypothesis postfix (lowerToken:string) (x:TrieSearchResult<LemmaInfo list>) =
+        let form = x.Prefix.Reverse()
+        x.Record 
+        |> Seq.map (fun r -> 
+                    let record = r.GetFinal()
+                    let replaced = form.Replace(postfix,"")
+                    let replacer = if String.IsNullOrWhiteSpace replaced  
+                                        then record.Lemma                                    
+                                        else record.Lemma.Replace(replaced,"") 
+                    let lemma = lowerToken.Replace(postfix, replacer)
+                    LemmaInfo(0, lemma, record.Gr, 0)
+                    )
+
+    let MakePredictions (records:TrieSearchResult<LemmaInfo list>[]) (prefix:string) (lowerToken:string) =
+        let postfix = prefix.Reverse()
+        records |> Seq.collect (fun x-> createLemmaHypothesis postfix lowerToken x)
+                |> Seq.toList
+
+    let makePredictionsByFull (records:TrieSearchResult<LemmaInfo list>[]) (prefix:string) (lowerToken:string) =
+        let postfix = prefix.Reverse()
+        let appendix = lowerToken.Replace (postfix, "")
+        records |> Seq.collect (fun x-> x.Record |> Seq.map ((fun l -> l.GetFinal()) >> (fun l -> LemmaInfo(0,appendix + l.Lemma, l.Gr,0))))
+                |> Seq.toList
