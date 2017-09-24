9cec6103038f3658fddc2397aed48723cb7cd72a
Alexander Kozko <Alexander Kozko>
2017-09-24 17:40:14 +0500

    test file

--- Sources/NetMorpher/Script.fsx
+++ Sources/NetMorpher/Script.fsx
@@ -1,6 +1,8 @@
 #load "../../references.fsx"
 open NetMorpher.Analyser
 open MBrace.FsPickler
+open System
+open System.Text.RegularExpressions
 open System.IO
 open System.Collections.Generic
 open NetMorpher.Persistent
@@ -9,4 +11,15 @@ open NetMorpher.Structures.TrieMap
 
 let morpher = NetMorpher.Load(DataFolder.BinaryLoader)
 
-printf "%s" <| morpher.LemmatizeToken "хомякового"
+let lemmatize words = 
+        words |> Array.map morpher.LemmatizeToken
+
+let words = File.ReadAllLines <| DataFolder.GetPath "onegin.txt" 
+                |> Seq.filter (fun w -> not <| Regex.IsMatch(w, @"\P{IsCyrillic}"))
+                |> Seq.toArray
+
+let words = words |> Array.append words |> Array.append words |> Array.append words |> Array.append words |> Array.append words 
+
+#time
+lemmatize words
+#time
